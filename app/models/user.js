29f5bdd7e5330bc16ae5fa671b204ed069b46a46
Sharad Jain <shaanjain@gmail.com>
2016-02-22 13:38:46 -0600

    formatting

--- app/models/user.js
+++ app/models/user.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
+  first_name: String,
+  last_name: String,
+  email: String,
 });
 
 mongoose.model('User', UserSchema);
