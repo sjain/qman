7dec15cd6607528d34e2840335abf1bc0dab13c9
Sharad Jain <shaanjain@gmail.com>
2016-04-23 21:58:07 -0500

    use $http service and fetch users in angular app.

--- client/js/layout/shell_controller.js
+++ client/js/layout/shell_controller.js
@@ -6,14 +6,21 @@
     .controller('ShellController', ShellController);
 
   /* @ngInject */
-  function ShellController($rootScope) {
+  function ShellController($rootScope, $http) {
 
     var vm = this;
     loadData();
 
     function loadData() {
       vm.hello = "Hello from ShellController";
+      $http({
+        method: 'GET',
+        url: '/users/json'
+      }).then(function(response) {
+        vm.users = response.data;
+      }, function() {
+        console.log("error fetching users");
+      });
     }
-
   }
 })();
